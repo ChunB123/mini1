69560f06481eff005677e6cc284ef8ab6a118b35
Michael Xu <jiazheng.xu@mail.mcgill.ca>
2023-10-05 01:48:44 -0400

    TODO: metrics fo CV

--- main.py
+++ main.py
@@ -6,59 +6,12 @@ from sklearn.datasets import load_breast_cancer
 import sklearn.model_selection
 import matplotlib.pyplot as plt
 import random
+import seaborn as sns
 
 from ucimlrepo import fetch_ucirepo
 
 
-def train_model_using_grad_descent(X, y, alpha=0.1, max_iter=100):
-    beta = np.zeros(X.shape[1] + 1)
-    L_vals = []
-    for _ in tqdm(range(max_iter)):
-        beta = beta - alpha * grad_L(X, y, beta)
-        L_vals.append(eval_L(X, y, beta))
-    return beta, L_vals
-
-
-def train_model_using_stochastic_grad_descent(X, y, alpha=0.1, epoch=500):
-    beta = np.zeros(X.shape[1] + 1)
-    L_vals = []
-    for _ in tqdm(range(epoch)):
-        indexList = random.sample(range(X.shape[0]), X.shape[0])
-        for i in indexList:
-            beta = beta - alpha * grad_L(X[i], y[i], beta)
-        L_vals.append(eval_L(X, y, beta))
-    return beta, L_vals
-
-
-def draw(datas, legends, xlabel, ylabel, title):
-    # create a new figure
-    fig, ax = plt.subplots()
-    # Plot each curve on the axes object
-    for index, data in enumerate(datas):
-        ax.plot(data, label=str(legends[index]))
-
-    # Add a legend to the axes object
-    ax.legend()
-
-    # Set the x-axis and y-axis labels
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-
-    # Set the title of the figure
-    ax.set_title(title)
-
-    # Display the figure
-    plt.show()
-
-
-def draw_loss(L_vals):
-    plt.plot(list(range(len(L_vals))), L_vals, '-o', color='blue')
-    plt.xlabel('Iterations')
-    plt.ylabel('Loss')
-    plt.title('Iterations VS Loss')
-    plt.show()
-
-
+# utils
 def normalize_features(df, method='z-score'):
     # Extract the feature columns (excluding 'MEDV')
     feature_cols = [col for col in df.columns if col != 'MEDV']
@@ -85,31 +38,18 @@ def normalize_features(df, method='z-score'):
     return normalized_df
 
 
-def sigmoid(u):
-    # use identity to prevent overflow
-    if isinstance(u, (int, float)):
-        if u >= 0:
-            return 1.0 / (1.0 + np.exp(-u))
-        else:
-            return np.exp(u) / (1.0 + np.exp(u))
-    else:
-        positive_mask = u >= 0
-        output = np.array([0 for x in range(len(u))]).astype("float64")
-        output[positive_mask] = 1 / (1 + np.exp(-u[positive_mask]))
-        output[~positive_mask] = np.exp(u[~positive_mask]) / (1 + np.exp(u[~positive_mask]))
-        return output
+def draw_loss(L_vals):
+    plt.plot(list(range(len(L_vals))), L_vals, '-o', color='blue')
+    plt.xlabel('Iterations')
+    plt.ylabel('Loss')
+    plt.title('Iterations VS Loss')
+    plt.show()
 
 
 def softmax(u):
     # return np.exp(u) / np.sum(np.exp(u))
     # use log_softmax instead to prevent overflow
     return np.exp(u - np.max(u) - np.log(np.sum(np.exp(u - np.max(u)))))
-
-
-def binary_cross_entropy(p, q, eps=1e-10):
-    # prevent ln(0)
-    q = np.clip(q, eps, 1 - eps)
-    return -p * np.log(q) - (1 - p) * np.log(1 - q)
 
 
 def cross_entropy(p, q, eps=1e-10):
@@ -120,17 +60,7 @@ def cross_entropy(p, q, eps=1e-10):
 def Xhat(X):
     if len(X.shape) == 1:
         return np.insert(X, 0, 1)
-    return np.column_stack((np.ones((X.shape[0], 1)),X))
-
-
-def grad_L(X, y, beta):
-    if len(X.shape) == 1:
-        return (sigmoid(Xhat(X) @ beta) - y) * Xhat(X)
-    return np.average(np.array([(sigmoid(x @ beta) - y[index]) * x for index, x in enumerate(Xhat(X))]), axis=0)
-
-
-def eval_L(X, y, beta):
-    return np.average([binary_cross_entropy(y[index], sigmoid(xi @ beta)) for index, xi in enumerate(Xhat(X))])
+    return np.column_stack((np.ones((X.shape[0], 1)), X))
 
 
 def grad_L_m(X, y, beta):
@@ -142,17 +72,6 @@ def grad_L_m(X, y, beta):
 def eval_L_m(X, y, beta):
     return np.average([cross_entropy(y[index], softmax(xi @ beta)) for index, xi in enumerate(Xhat(X))])
 
-def train_model_using_stochastic_grad_descent_multi(X, y, alpha=0.01, epoch=500):
-    beta = np.zeros((X.shape[1] + 1, y.shape[1])).astype("float64")
-    L_vals = []
-    for _ in tqdm(range(epoch)):
-        indexList = random.sample(range(X.shape[0]), X.shape[0])
-        for i in indexList:
-            beta = beta - alpha * grad_L_m(X[i], y[i], beta)
-        # use cross-entropy
-        L_vals.append(eval_L_m(X, y, beta))
-    return beta, L_vals
-
 
 def train_model_using_grad_descent_multi(X, y, alpha, max_iter):
     beta = np.zeros((X.shape[1] + 1, y.shape[1])).astype("float64")
@@ -161,6 +80,100 @@ def train_model_using_grad_descent_multi(X, y, alpha, max_iter):
         beta = beta - alpha * grad_L_m(X, y, beta)
         L_vals.append(eval_L_m(X, y, beta))
     return beta, L_vals
+
+
+def MSE(Yhat, Y):
+    n = Yhat.shape[0]
+    res = (Yhat - Y) ** 2
+
+    return sum(res) / n
+
+
+def L2gradient(X, Y, Yhat):
+    # Also applicable for Log gradient
+    return (X.T @ (Yhat - Y)) / X.shape[0]
+
+
+def onehot(arr):
+    unique_classes, inverse = np.unique(arr, return_inverse=True)
+    arr = np.zeros((len(arr), len(unique_classes)))
+    arr[np.arange(len(arr)), inverse] = 1
+
+    return arr
+
+
+def zerohot(df, axis=0):
+    # input: array or dataframe that is one-hot encoded or simply categorical, the axis along which we decode (default = 0)
+    # returns: the argmax value of the one-hot encoded columns/axes
+    unhot = np.argmax(df, axis)
+    return unhot
+
+
+def cMatrix_log(Yhat, Y, axis=(0, 1), onehot=False):
+    # logistic regression confusion matrix
+    # Inputs Yhat: predicted values, Y: expected values. Yhat and Y can be either lists, np.arrays.
+    # Axis tuple to precise along which axis of the matrix we decode for Yhat, Y respectively
+    # returns df of confusion matrix
+    if not onehot:
+        d = {'Predicted Values': Yhat, 'Actual Values': Y}
+        df = pd.DataFrame(data=d)
+
+        cMat = pd.crosstab(df["Predicted Values"], df['Actual Values'])
+        fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+        ax.set_title('Confusion Matrix for Logistic Regression')
+        sns.heatmap(cMat, annot=True)
+
+    else:
+        d = {'Predicted Values': zerohot(Yhat, axis[0]), 'Actual Values': zerohot(Y, axis[1])}
+        df = pd.DataFrame(data=d)
+
+        cMat = pd.crosstab(df["Predicted Values"], df['Actual Values'])
+        fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+        ax.set_title('Confusion Matrix for Logistic Regression')
+        sns.heatmap(cMat, annot=True)
+
+    # The code that follows will only run after all plots have been drawn
+    plt.show()
+    return cMat
+
+
+def printMetrics(cMat):
+    metricsList = metrics_cal(cMat)
+    print("Metrics for avg_accuracy: %f, avg_precision: %f, avg_recall: %f, avg_F1: %f" % (
+        metricsList[0], metricsList[1], metricsList[2], metricsList[3]))
+
+
+def metrics_cal(cMat):
+    # Initialize the avg of metrics
+    accuracy = precision = recall = F1 = 0
+    for idx in range(cMat.shape[0]):
+        TP = np.sum(cMat.iloc[idx, idx])
+        FP = np.sum(cMat.iloc[idx, :]) - TP
+        FN = np.sum(cMat.iloc[:, idx]) - TP
+        TN = np.sum(cMat.sum()) - TP - FP - FN
+
+        accuracy += (TP + TN) / (TP + TN + FP + FN)
+        precision += TP / (TP + FP)
+        recall += TP / (TP + FN)
+        F1 += 2 * precision * recall / (precision + recall)
+    return [num/cMat.shape[0] for num in [accuracy, precision, recall, F1]]
+
+
+
+def metricsWrapper(y_train_hat, y_train, y_test_hat, y_test):
+    print("Overall Accuracy for training set: ", str(np.average(y_train_hat == y_train)))
+    print("Performance metrics for training set: ")
+    printMetrics(cMatrix_log(y_train_hat, y_train))
+
+    print("==========================")
+    print("Overall Accuracy for test set: ", str(np.average(y_test_hat == y_test)))
+    print("Performance metrics for test set:")
+    printMetrics(cMatrix_log(y_test_hat, y_test))
+
+
+def split_indices(indices, N=5):
+    np.random.shuffle(indices)
+    return np.array_split(indices, N)
 
 
 # Multiclass Gradient descent
@@ -186,20 +199,6 @@ class LogisticRegression:
         y_pred = [np.argmax(yi) + 1 for yi in (Xhat(X_test) @ self.beta)]
         return y_pred
 
-# Split Dataframe to N folds
-def split_dataframe(df, n_folds=5, random_seed=10):
-    df_shuffled = df.sample(frac=1, random_state=random_seed).reset_index(drop=True)
-    fold_size = len(df_shuffled) // n_folds
-
-    folds = []
-    for i in range(n_folds):
-        if i == n_folds - 1:
-            # Store remaining data to last fold
-            folds.append(df_shuffled.iloc[i*fold_size:])
-        else:
-            folds.append(df_shuffled.iloc[i*fold_size:(i+1)*fold_size])
-
-    return folds
 
 # Fetch dataset
 wine = fetch_ucirepo(id=109)
@@ -218,14 +217,25 @@ X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y
 
 model = LogisticRegression(alpha=0.01, max_iter=70)
 logReg = model.fit(X_train, y_train)
-#draw_loss(logReg.L_vals)
-y_pred = model.predict(X_test)
-print("Accuracy using Gradient Descent: ", str(np.average(y_pred == y_test['class'].values)))
+# draw_loss(logReg.L_vals)
+# performance metrics
+y_train_hat = model.predict(X_train)
+y_test_hat = model.predict(X_test)
 
-folds = split_dataframe(wine_df)
+metricsWrapper(y_train_hat, y_train['class'].values, y_test_hat, y_test['class'].values)
 
-for idx, fold in enumerate(folds):
-    data_train = pd.concat(folds[:idx] + folds[idx+1:])
-    data_val = fold
+metrics = [[], []]
+folds_indices = split_indices(wine_df.index.tolist())
+for idx, indices in enumerate(split_indices(wine_df.index.tolist())):
+    X_train = X.iloc[np.concatenate(folds_indices[:idx] + folds_indices[idx + 1:])]
+    X_test = X.iloc[indices.tolist()]
+    y_train = y.iloc[np.concatenate(folds_indices[:idx] + folds_indices[idx + 1:])]
+    y_test = y.iloc[indices.tolist()]
+
     model = LogisticRegression(alpha=0.01, max_iter=70)
     logReg = model.fit(X_train, y_train)
+    # draw_loss(logReg.L_vals)
+    y_train_hat = model.predict(X_train)
+    y_test_hat = model.predict(X_test)
+    #metrics[0].append(metrics_cal(cMatrix_log(y_train_hat, y_train)))
+    #metrics[1].append(metrics_cal(cMatrix_log(y_test_hat, y_test)))
